5e12769e8715cf72feafbff07cf6cbf18d65f004
Joel Marco Quiroga Poma <joelqp1@gmail.com>
2023-06-02 12:52:27 +0200

    v7

--- prueba_1servo.py
+++ prueba_1servo.py
@@ -1,7 +1,9 @@
 import time
 from adafruit_pca9685 import PCA9685
-
+import board
+import busio
 # Initialize the PCA9685 controller
+i2c = busio.I2C(board.SCL, board.SDA)
 pca = PCA9685()
 pca.frequency = 50  # Set the PWM frequency (adjust if necessary)
 
