fd80ca477f1d641d6c55f4613b007b3cd1fad9a5
Joel Marco Quiroga Poma <joelqp1@gmail.com>
2023-06-02 12:54:24 +0200

    v8

--- prueba_1servo.py
+++ prueba_1servo.py
@@ -4,7 +4,7 @@ import board
 import busio
 # Initialize the PCA9685 controller
 i2c = busio.I2C(board.SCL, board.SDA)
-pca = PCA9685()
+pca = PCA9685(i2c)
 pca.frequency = 50  # Set the PWM frequency (adjust if necessary)
 
 def set_servo_angle(channel, angle):
