6cb37947723f113aebb368e5f4e45c10f699d6d8
Joel Marco Quiroga Poma <joelqp1@gmail.com>
2023-06-02 12:29:01 +0200

    v324

--- servo_control.py
+++ servo_control.py
@@ -9,10 +9,10 @@ from time import *
 
 
 kit.servo[0].angle = 140
-kit.servo[1].angle = 140
-kit.servo[2].angle = 130
-kit.servo[3].angle = 160
-kit.servo[4].angle = 140
+# kit.servo[1].angle = 140
+# kit.servo[2].angle = 130
+# kit.servo[3].angle = 160
+# kit.servo[4].angle = 140
 
 
 cap = cv2.VideoCapture(0)
